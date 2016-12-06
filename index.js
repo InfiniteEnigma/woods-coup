f7b8089c5d2d51553bdc687929462a31ce497ef8
ahan <ahan@ennuic.com>
2016-12-05 20:37:33 -0500

    test

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
+var gameActive = false
+var players = [];
+
 bot.on("message", msg => {
-    if (msg.content.startsWith("ping")) {
-        msg.channel.sendMessage("pong!");
+
+  let prefix = "!";
+
+  if (!msg.content.startsWith(prefix)) return;
+
+  //Test bot response.
+  else if (msg.content.startsWith(prefix + "ping")) {
+    msg.channel.sendMessage("Hi " + msg.author + "!");
+  }
+
+  //Joining a game if game is inactive
+  else if (((msg.content.startsWith(prefix + "join")) || (msg.content.startWith(prefix + "j"))) && (gameActive == false)) {
+    var temp = players.indexOf(msg.author);
+    if (temp == -1) {
+      players = players.push(msg.author);
+      msg.channel.sendMessage(msg.author + " has joined the game!");
     }
+    else {
+      msg.channel.sendMessage("Error joining the game! Perhaps you are already entered?");
+    }
+  }
+
+  //Leaving a game if game is inactive
+  else if (((msg.content.startsWith(prefix + "leave")) || (msg.content.startWith(prefix + "l"))) && (gameActive == false)) {
+    var temp = players.indexOf(msg.author);
+    if (temp == -1) {
+      msg.channel.sendMessage("You are not in the game!");
+    }
+    if (temp > -1) {
+      players = players.splice(pos, temp);
+      msg.channel.sendMessage("You are have been removed from the game!");
+    }
+  }
+
 });
 
 bot.on('ready', () => {
