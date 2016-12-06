f619d08bc33a5b072b285b12b13dd2f03569758c
InfiniteEnigma <haichen16@gmail.com>
2016-12-06 18:11:08 -0500

    finally done the shuffle part

--- game.js
+++ game.js
@@ -18,41 +18,27 @@ function coupGame(playerNum, ambassadorInquisitor) {
     cardCount = 3;
   }
   else {
-    for (let i = 1; i <= playerNum; i++) {
-      cardCount = 3;
+    for (let i = 1; i <= playerNum; i++){
+      playerMoney.push(2);
     }
-    for (let i = 1; i <= cardCount; i++) {
-      deck.push("Captain");
-      deck.push("Duke");
-      deck.push("Contessa");
-      deck.push("Assassin");
-      if (ambassadorInquisitor == 1) {
-        deck.push("Ambassador");
-      }
-      else if (ambassadorInquisitor == 2) {
-        deck.push("Inquisitor");
-      }
-      for (let i = 1; i <= playerNum; i++) {
-        playerMoney.push(2);
-      }
-      cardCount = 3 + Math.ceil((playerNum-5)/3);
-    }
-    for (let i = 1; i <= cardCount; i++) {
-      deck.push("Captain");
-      deck.push("Duke");
-      deck.push("Contessa");
-      deck.push("Assassin");
-      if (ambassadorInquisitor == 1) {
-        deck.push("Ambassador");
-      }
-      for (let i = 1; i <= 50; i++) {
-        deck = shuffleArray(deck);
-      }
-    }
-    for (var c = 1; c < 50; c++) {
-      deck = shuffleArray(deck);
-    }
-    console.log(deck);
+    cardCount = Math.ceil((playerNum-5)/3) + 3;
   }
+  for (let i = 1; i <= cardCount; i++) {
+    deck.push("Captain");
+    deck.push("Duke");
+    deck.push("Contessa");
+    deck.push("Assassin");
+    if (ambassadorInquisitor == 1) {
+      deck.push("Ambassador");
+    }
+    else if (ambassadorInquisitor == 2) {
+      deck.push("Inquisitor");
+    }
+  }
+  for (let i = 1; i <= 50; i++) {
+    deck = shuffleArray(deck);
+  }
+  console.log(deck);
+  console.log(deck.length);
 }
-coupGame(3,1);
+coupGame(18,1);
